62f8c589745ec6e14054a019a8732514ae7a20bf
Ciaran Harman <Ciaran.harman@gmail.com>
2023-03-09 15:01:04 -0500

    Converging!

--- PythonEquivalent/functions/models.py
+++ PythonEquivalent/functions/models.py
@@ -38,41 +38,41 @@ def data_generation(precip: List[float], \
     df = pd.DataFrame({"J":J,"Q":Q[1:]})
     df = df.dropna()
     return df['J'].values, df['Q'].values
-# %%
-# =================
-# Truth generation
-# =================
-# read data
-precip = pd.read_csv("precip.csv", index_col = 0)
-precip = precip['45'].values
-# define constant
-l = 100 # control the length of the time series
-delta_t = 1./24/60*15
-k = 1.
-#
-J,Q = data_generation(precip, delta_t, k , l)
-Q_true = Q.copy()
-J_true = J.copy()
-# pretend we know obs error
-theta_ipt = 0.254*delta_t
-theta_obs = 0.00005
-Q += np.random.normal(0, theta_obs,size = len(Q))
-J += np.random.uniform(0, theta_ipt,size = len(Q))
-df = pd.DataFrame({'J_true': J_true,'J_obs': J, 'Q_true': Q_true, 'Q_obs': Q})
-df = df[600:]
-df = df.reset_index()
+## %%
+## =================
+## Truth generation
+## =================
+## read data
+#precip = pd.read_csv("precip.csv", index_col = 0)
+#precip = precip['45'].values
+## define constant
+#l = 100 # control the length of the time series
+#delta_t = 1./24/60*15
+#k = 1.
+##
+#J,Q = data_generation(precip, delta_t, k , l)
+#Q_true = Q.copy()
+#J_true = J.copy()
+## pretend we know obs error
+#theta_ipt = 0.254*delta_t
+#theta_obs = 0.00005
+#Q += np.random.normal(0, theta_obs,size = len(Q))
+#J += np.random.uniform(0, theta_ipt,size = len(Q))
+#df = pd.DataFrame({'J_true': J_true,'J_obs': J, 'Q_true': Q_true, 'Q_obs': Q})
+#df = df[600:]
+#df = df.reset_index()
 
-plt.figure()
-plt.subplot(2,1,1)
-plt.plot(df['J_obs'],"*",label = "J obs")
-plt.plot(df['J_true'],label = "J true")
-plt.legend()
-plt.title('J')
-plt.subplot(2,1,2)
-plt.plot(df['Q_obs'],label = "Q obs")
-plt.plot(df['Q_true'],label = "Q true")
-plt.title('Q')
-plt.legend()
-plt.tight_layout()
-df.to_csv("Dataset.csv")
-# %%
+#plt.figure()
+#plt.subplot(2,1,1)
+#plt.plot(df['J_obs'],"*",label = "J obs")
+#plt.plot(df['J_true'],label = "J true")
+#plt.legend()
+#plt.title('J')
+#plt.subplot(2,1,2)
+#plt.plot(df['Q_obs'],label = "Q obs")
+#plt.plot(df['Q_true'],label = "Q true")
+#plt.title('Q')
+#plt.legend()
+#plt.tight_layout()
+#df.to_csv("Dataset.csv")
+## %%
