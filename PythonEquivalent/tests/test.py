7dcb28cbee31dbd69ae53fc17d608cb917dc2b51
Esther Xu <fxu12@jhu.edu>
2023-03-14 13:55:22 -0400

    temp update after getting a test

---
+++ PythonEquivalent/tests/test.py
@@ -0,0 +1,60 @@
+# %%
+import pytest
+import pandas as pd
+
+from functions.utils import plot_MLE
+from model import run_sMC, run_pMCMC
+import numpy as np
+# %%
+class SetTest:
+    def __init__(self, filename = "Dataset.csv", T = 50, interval = 1, k = 1,delta_t = 1./24/60*15,\
+                 theta_ipt = 0.254*1./24/60*15,theta_obs = 0.00005,N = 50):
+        df = pd.read_csv(filename, index_col= 0)
+        self.T = T
+        self.interval = interval
+        self.df = df[:self.T]
+        self.J_obs = df['J_obs'][::self.interval].values
+        self.Q_obs = df['Q_obs'][::self.interval].values
+        self.theta = k
+        self.K = len(self.J_obs)
+        self.delta_t = delta_t
+        self.delta_t *= self.interval
+        # estimation inputs
+        self.N = N
+        self.sig_v = theta_ipt
+        self.sig_w = theta_obs*10
+    # ==================
+    # sMC
+    # ==================
+    def test_sMC(self): 
+        """
+        Test if sMC is working as expected
+        """
+        self.X, self.A, self.W, self.R = run_sMC(self.J_obs, self.Q_obs, self.theta, self.delta_t,self.N, self.sig_v,self.sig_w)
+        MLE = plot_MLE(self.X, self.A, self.W, self.R,self.K,self.df,self.J_obs, self.Q_obs,self.sig_v,self.sig_w,left = 0, right = 30)
+        truth = self.df['Q_true']
+        sum_squared_residuals = sum((MLE - truth)**2)
+        return np.sqrt(sum_squared_residuals/len(sum_squared_residuals))
+    # %%
+    # ==================
+    # pMCMC
+    # ==================
+    def test_pMCMC(self): 
+        """
+        Test if pMCMC is working as expected
+        """
+        self.X, self.A, self.W, self.R = run_pMCMC(self.theta, self.sig_v,self.sig_w, self.X, self.W, self.A,self.R, self.J_obs,self.Q_obs, self.delta_t)
+        MLE = plot_MLE(self.X, self.A, self.W, self.R,self.K,self.df,self.J_obs, self.Q_obs,self.sig_v,self.sig_w,left = 0, right = 30)
+        truth = self.df['Q_true']
+        sum_squared_residuals = sum((MLE - truth)**2)
+        return np.sqrt(sum_squared_residuals/len(sum_squared_residuals))
+class TestFunctions(unittest.TestCase):
+    def run_test(self):
+        result_std = SetTest(filename = "Dataset.csv", T = 50, interval = 1, k = 1,delta_t = 1./24/60*15,\
+                 theta_ipt = 0.254*1./24/60*15,theta_obs = 0.00005,N = 50)
+        self.assertLess(result_std.test_sMC(), self.sig_w/10,"Variance too large")
+        self.assertLess(result_std.test_pMCMC(), self.sig_w/10,"Variance too large")
+
+
+if __name__ == '__main__':
+    unittest.main()
