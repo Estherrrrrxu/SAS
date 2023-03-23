11bfb4abe13583319d8d9cd49290d09de51c11b3
Esther Xu <fxu12@jhu.edu>
2023-03-23 10:59:52 -0400

    need polish pMCMC and param selection

--- PythonEquivalent/run_this.py
+++ PythonEquivalent/run_this.py
@@ -14,6 +14,35 @@ interval = 1
 df = df[:T:interval]
 # %%
 default_model = SSModel(data_df=df)
+# %%
+# CHECK sMC
+default_model.N = 10
+# set default theta
+theta_init = {
+    'to_estimate': {'k':{"prior_dis": "normal", "prior_params":[1.2,0.3], 
+                            "update_dis": "normal", "update_params":[0.05]},
+                    'output_uncertainty':{"prior_dis": "uniform", "prior_params":[0.00005,0.0005], 
+                            "update_dis": "normal", "update_params":[0.00005]}},
+    'not_to_estimate': {'input_uncertainty': 0.254*1./24/60*15}
+}
+# process theta
+default_model._theta_init = theta_init
+default_model._process_theta()
+X,A,W,R = default_model.run_sMC([1.,0.00005])
+from functions.utils import plot_MLE
+plot_MLE(X,A,W,R,default_model.K,df,default_model.influx, default_model.outflux,left = 0, right = default_model.K)
+# %%
+# AA = np.broadcast_to(A, (default_model.D, *A.shape))
+# WW = np.zeros((default_model.D,default_model.N))
+# XX = np.zeros((default_model.D,default_model.N,default_model.K+1))
+# RR = np.zeros((default_model.D,default_model.N,default_model.K))
+# default_model.run_pMCMC([1.,0.00005], X[np.newaxis,:,:],A[np.newaxis,:,:],W[np.newaxis,:],R[np.newaxis,:,:])
+
+
+
+
+# %%
+
 default_model.run_pGS_SAEM(num_particles=10,num_params=5, len_MCMC=15)
 # %%
 plt.figure()
