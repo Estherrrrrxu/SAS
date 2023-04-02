88d88b5e6b3ff087b5edb03fd4b5cd8ef231d0f2
Esther Xu <fxu12@jhu.edu>
2023-04-02 19:50:54 -0400

    It's working

--- PythonEquivalent/run_this.py
+++ PythonEquivalent/run_this.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 
 import matplotlib.pyplot as plt
-
+from functions.utils import plot_MLE
 from functions.link import ModelLink
 from functions.estimator import SSModel, _inverse_pmf
 # %%
@@ -18,46 +18,16 @@ default_model = SSModel(model_link)
 
 # %%
 state = default_model.run_sequential_monte_carlo([1.,0.00005])
-
+# plot_MLE(state,df,left = 0, right = 50)
 # %%
-def plot_MLE(state,df,left = 0, right = 500):
-    X = state.X
-    A = state.A
-    W = state.W
-    R = state.R
-    J_obs = df['J_obs'].values
-    Q_obs = df['Q_obs'].values
-    K = len(J_obs)
-
-    B = np.zeros(K+1).astype(int)
-    B[-1] = _inverse_pmf(W,A[:,-1], num = 1)
-    for i in reversed(range(1,K+1)):
-        B[i-1] =  A[:,i][B[i]]
-    MLE = np.zeros(K+1)
-    MLE_R = np.zeros(K)
-    for i in range(K+1):
-        MLE[i] = X[B[i],i]
-    for i in range(K):
-        MLE_R[i] = R[B[i+1],i]   
-    T = len(X[0])-1
-    # ------------
-    plt.figure()
-    plt.subplot(2,1,1)
-    plt.plot(df['J_true'],label = "J true")
-    plt.plot(J_obs,"*",label = "J obs")
-    plt.plot(np.linspace(0,T,len(MLE_R)),MLE_R,'r:', label = "One Traj/MLE")
-    plt.legend(frameon = False)
-    plt.legend(frameon = False)
-    plt.xlim([left,right])
-    plt.title('J')
-
-    plt.subplot(2,1,2)
-    plt.plot(Q_obs,".", label = "Observation")
-    plt.plot(df['Q_true'],'k', label = "Hidden truth")
-    plt.plot(np.linspace(0,T,len(MLE)-1),MLE[1:],'r:', label = "One Traj/MLE")
-    plt.legend(frameon = False)
-    # plt.title(f"sig_v {round(sig_v,5)}, sig_w {sig_w}")
-    plt.xlim([left,right])
-    plt.tight_layout()
-    return MLE
-plot_MLE(state,df,left = 0, right = 50)
+state = default_model.run_particle_MCMC(state,theta = [1.,0.00005])
+# plot_MLE(state,df,left = 0, right = 50)
+# %%
+default_model.run_particle_Gibbs_SAEM(
+    num_parameter_samples=10,
+    len_MCMC=20)
+# %%
+fig, ax = plt.subplots(2,1,figsize=(10,5))
+ax[0].plot(default_model.theta_record[:,0])
+ax[1].plot(default_model.theta_record[:,1])
+# %%
