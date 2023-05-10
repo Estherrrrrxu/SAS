1ecc20ce848eed0f9bde25ae446d26b6e10e73e8
Esther Xu <fxu12@jhu.edu>
2023-05-09 20:35:26 -0400

    good on testing for other time series. Next, bound

--- PythonEquivalent/run_linear_reservoir.py
+++ PythonEquivalent/run_linear_reservoir.py
@@ -7,16 +7,19 @@ from functions.utils import plot_MLE
 import matplotlib.pyplot as plt
 # %%
 df = pd.read_csv("Data/linear_reservoir.csv", index_col= 0)
-st = 0
-et = 50
-interval = 1
-df = df[st:et:interval]
+st = 300
+et = 400
+interval = 5
+df = df[st:et]
+df_obs = df[::interval]
+
 
 fig, ax = plt.subplots(2, 1, figsize=(8,5))
 ax[0].bar(df.index, df['J_true'], 
           width = 1, color = 'b', alpha = 0.5, 
           label = 'Truth')
-ax[0].plot(df['J_obs'], '*', color = 'b',label = 'Observation')
+ax[0].plot(df_obs['J_obs'], '.', color = 'r', alpha = 0.5, 
+           label = 'Observation')
 ax[0].invert_yaxis()
 ax[0].set_ylabel("Precipitation [mm]")
 ax[0].legend(frameon = False)
@@ -24,13 +27,13 @@ ax[0].set_xticks([])
 ax[0].set_title("Preciptation")
 
 ax[1].plot(df['Q_true'], color = 'b', alpha = 0.5, label = 'Truth')    
-ax[1].plot(df['Q_obs'], '*', color = 'b', label = 'Observation')
+ax[1].plot(df_obs['Q_obs'], '.', color = 'r', label = 'Observation')
 ax[1].set_ylabel("Discharge [mm]")
 ax[1].set_xlabel("Time [day]")
 ax[1].legend(frameon = False)
 ax[1].set_title("Discharge")
 
-
+df_obs.index = range(len(df_obs))
 # %%
 # define theta_init
 theta_init = {
@@ -45,7 +48,7 @@ theta_init = {
 }
 # initialize model interface settings
 model_interface = ModelInterface(
-    df = df,
+    df = df_obs,
     customized_model = None,
     theta_init = theta_init,
     config = None,
@@ -67,15 +70,16 @@ model_interface = ModelInterface(
 model = SSModel(
     model_interface = model_interface,
     num_parameter_samples = 10,
-    len_parameter_MCMC = 15,
+    len_parameter_MCMC = 50,
     learning_step = 0.75
 )
 model.run_particle_Gibbs_AS_SAEM()
 # %%
 fig, ax = plt.subplots(2,1,figsize=(10,5))
-ax[0].plot(model.theta_record[:,0])
-ax[1].plot(model.theta_record[:,1])
+ax[0].plot(model.theta_record[:,0]/interval)
+ax[1].plot(model.theta_record[:,1]/interval)
 ax[0].set_ylabel(r"$k$")
+ax[0].set_xticks([])
 ax[1].set_ylabel(r"$\theta_{obs}$")
 ax[1].set_xlabel("MCMC Chain length")
 
